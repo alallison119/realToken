42daec25acbc4ca02bd37aea6dd60433ea8c014c
alallison119 <alalison119@gmail.com>
2022-11-26 20:04:35 -0500

    ss

---
+++ contracts/ICO.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ICO is AccessControl{
+    using SafeMath for uint256;
+    bytes32 public constant INVESTOR_ROLE = keccak256("INVESTOR_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    //address payable immutable owner;
+    //address payable immutable devAddress;
+
+    address payable owner;
+    address payable private devAddress;
+    address[] minters_;
+    uint256 maxSupply_;
+    uint256 timeLock=18000000000;
+    IERC20 eftt;
+    uint256 maxICO = 100000 *10**18;
+    uint256 SoldInMetis;
+    uint256 SoldEFTT;
+    uint256 ratioMetis =1;
+    mapping(uint => uint) timeLocks;
+
+
+
+    constructor(address _eftt, address _dev) payable {
+        //below might have to obfuscate with for loop
+        _grantRole(INVESTOR_ROLE, _dev);
+        _grantRole(BURNER_ROLE, _dev);
+        owner = payable(msg.sender);
+        eftt = IERC20(_eftt);
+        devAddress = payable(_dev);
+        timeLocks[0] = 111;
+        timeLocks[1] = 222;
+        timeLocks[2] = 333;
+            }
+
+    event Log(string func);
+    event Sold(uint);
+ 
+    modifier _timeCheck(){
+        require(block.timestamp < timeLock,"ICO is over :( ");
+        _;
+    }
+    modifier _onlyOwner(){
+        require(owner == msg.sender,"You're not the owner");
+        _;
+    }
+    
+    receive() external payable {
+        //add in if just metis sent to contract
+        emit Log("fallback hit");
+        buy();
+    }
+
+
+    function buy() public payable _timeCheck returns(bool){
+        require(msg.value > 0,"them/they please send metis");
+        uint256 buyAmount = conversion(msg.value);
+        require(buyAmount + SoldEFTT < maxICO, "exceeds the total for sale");
+        eftt.allowance( address(this),msg.sender);
+        eftt.transfer(msg.sender, buyAmount);
+        SoldEFTT = buyAmount + SoldEFTT;
+        emit Sold(buyAmount);
+        return true;
+    }
+
+    function withdraw(uint256 _amnt) onlyRole(INVESTOR_ROLE) public {
+        if (timeLocks[2]> block.timestamp){
+            payable(msg.sender).transfer(_amnt);
+        } else if (timeLocks[1]> block.timestamp){
+            //uint256 converted = soldPercent();
+            payable(msg.sender).transfer(_amnt);
+        } else if (timeLocks[0]> block.timestamp){
+            payable(msg.sender).transfer(_amnt);
+        }
+        
+    }
+
+    function endICO() public _timeCheck onlyRole(BURNER_ROLE){
+        
+
+    }
+
+    function conversion(uint256 _amnt) private view returns(uint256){
+        return(_amnt.mul(ratioMetis));
+    }
+
+    function soldPercent() private view returns(uint256){
+        return SoldEFTT.div(maxICO);
+    }
+}
